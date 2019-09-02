85bf1bea1bb4299151158c7d3637ec96e2ad7379
AldoDiaz01 <vitome77@hotmail.com>
2019-09-02 16:54:01 -0500

    greet feature

---
+++ src/app/greet/greet.spec.ts
@@ -0,0 +1,6 @@
+import { greet } from './greet';
+describe ('greet', () => { //greet suit
+    it ('should include the name in the message '; () => {
+        expect(greet('aldo')).toContain('aldo');
+    })
+})
