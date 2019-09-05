3b59af4626f74d0f9c23ade9dd2a6bce39adc59e
pandasneezing <CharoFueGa@hotmail.com>
2019-09-05 10:08:13 -0500

    fixed currencies

--- src/app/greet/greet.spec.ts
+++
@@ -1,6 +0,0 @@
-import { greet } from './greet';
-describe ('greet', () => { //greet suit
-    it ('should include the name in the message '; () => {
-        expect(greet('aldo')).toContain('aldo');
-    })
-})
